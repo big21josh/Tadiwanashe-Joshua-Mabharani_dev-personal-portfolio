7fbdb5dc7eb7e2c91f41dadcbfbf1585720e2f79
BIG21 <big21josh@gmail.com>
2026-08-05 13:05:40 +0200

    experience Array

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,4 +1,15 @@
-const experience
+const experience = [
+{
+  period: "June 2026 - Present",
+  role: "Junior Software Engineer",
+  company: "Uncommon.org",
+  description: "Building responsive web applications using React, JavaScript, and Python. Applying UX/UI design principles to create user-centered product experiences. Familiar with POS systems integration and retail tech solutions",
+  technologies: ["Software Development", "React", "Digital Marketing", "Python", "POS Systems"],
+  current: true,
+},
+
+
+]
 
 
 
